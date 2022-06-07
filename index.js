936c95d92b57b467a4250eae015de889da3a5f98
Emre Gundogan <emregundogan5@gmail.com>
2022-06-07 11:06:38 +0300

    added node and carousel files

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
-$(document).ready(function () {
-  $(".owl-carousel").owlCarousel();
-});
+// $(document).ready(function () {
+//   $(".owl-carousel").owlCarousel();
+// });
 
-$(".owl-carousel").owlCarousel({
+$(".owl-one").owlCarousel({
   loop: true,
-  margin: 10,
+  margin: 1,
   nav: true,
   responsive: {
     0: {
       items: 1,
     },
     600: {
-      items: 3,
+      items: 1,
     },
     1000: {
-      items: 5,
+      items: 1,
+    },
+  },
+});
+
+$(".owl-two").owlCarousel({
+  loop: true,
+  margin: 20,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+
+$(".owl-three").owlCarousel({
+  loop: true,
+  margin: 6,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
     },
   },
 });
